267517780072680041bcf4efbdb927c707d99dbb
muhammadeko <muhammadekoprasetyo29@gmail.com>
2022-05-11 23:29:35 +0700

    update to support 1.1.1-dev

--- src/main/java/me/exzork/gcauth/Config.java
+++ src/main/java/me/exzork/gcauth/Config.java
@@ -1,5 +1,8 @@
 package me.exzork.gcauth;
 
+import me.exzork.gcauth.utils.Authentication;
+
 public final class Config {
     public String Hash = "BCRYPT";
+    public String jwtSecret = Authentication.generateRandomString(32);
 }
